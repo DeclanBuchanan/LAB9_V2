64cb259ad8fe88cbe9857c33f03c0bad13c43d46
Jordanp11 <peterson_jordan@wheatoncollege.edu>
2025-04-16 16:24:40 -0400

    Student B part done

---
+++ geometry.cpp
@@ -0,0 +1,14 @@
+#include "geometry.h"
+
+
+float areaOfCircle(float radius) {
+  return 3.14 * radius * radius;
+}
+
+float areaOfRectangle(float l, float w) {
+  return l * w;
+}
+
+float volumeOfBox(float l, float w, float h) {
+  return l * w * h;
+}
