a3f7ffc06cbbe7a7cf6b323b188af1e67cb1aa43
markdavidson <markjdavidson24@gmail.com>
2025-04-16 16:23:11 -0400

    Mark's commit/contribution

---
+++ conversion.h
@@ -0,0 +1,9 @@
+#ifndef CONVERSION_H
+#define CONVERSION_H
+
+float celsiusToFahrenheit(float temp);
+float fahrenheitToCelsius(float temp);
+float inchesToCentimeters(float num);
+float centimetersToInches(float num);
+
+#endif //CONVERSION_H
